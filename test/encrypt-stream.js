caaa70bf25d2416c2ce4fd1279cfa306bec7a991
Pete Thomas <pete@xminusone.net>
2017-07-01 20:36:37 -0700

    Add test for readable stream.

--- test/encrypt-stream.js
+++ test/encrypt-stream.js
@@ -3,6 +3,7 @@ const Config = require('../lib/config.json'),
     fs = require('fs'),
     mocks = new (require('./mocks.js'))(),
     nodeStream = require('../lib/node-streams.js'),
+    stream = require('stream'),
     Package = require('../package.json'),
     transcoder = new (require('../lib/transcoder.js'))(),
     Wcrypt = require('../index.js');
@@ -14,7 +15,20 @@ const data = mocks.data,
         salt: mocks.salt
     }};
 
+function isReadableStream(obj) {
+    return obj instanceof stream.Stream &&
+        typeof (obj._read === 'function') &&
+        typeof (obj._readableState === 'object');
+}
+
 describe("Encrypt stream", function() {
+
+    it("Returns a readable stream", (done) => {
+        var wcrypt = new Wcrypt.cipher(testOptions);
+        const readme = fs.createReadStream(__dirname + '/../LICENSE');
+        expect(isReadableStream(readme)).toExist();
+        done();
+    });
 
     it("Returns expected buffer", (done) => {
         var hexEncoded = '',
